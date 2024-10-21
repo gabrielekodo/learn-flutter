59b22cbfebab1406e42702eb5834d46ecd7d0cdd
Gabriel Ekodo <ekodogabriel@gmail.com>
2024-10-21 19:30:49 +0300

    container scrren added

--- lib/screens/ContainerStyling.dart
+++ lib/screens/ContainerStyling.dart
@@ -6,12 +6,71 @@ class ContainerStyling extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lime,
+
       appBar: AppBar(
         title: const Text("Learn Container Styling"),
       ),
-        body: Center(
-            child: Image.asset("assets/flutter.png")
+        body: Column(
+
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(3)
+              ),
+              child: Text("This is a container with a blue background",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20
+              ),)
+            ),
+            Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(3)
+                ),
+                child: Text("This is a container with an orange background",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                  ),)
+            ),
+            Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(3)
+                ),
+                child: Text("This is a container with a deep purple background",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                  ),)
+            ),
+            Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(3)
+                ),
+                child: Text("This is a container with a red background",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                  ),)
+            )
+          ],
         )
+
+        // Center(
+        //     child: Image.asset("assets/flutter.png")
+        // )
     );
   }
 }
